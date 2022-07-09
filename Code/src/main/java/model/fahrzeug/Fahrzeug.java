ab4457b2a8e3bc39a03423720671cd8eba22601b
clemens1403 <Clemens.SRichter@t-online.de>
2022-07-09 11:01:58 +0200

    Fahrzeug klassen angelegt + referenzen und Attribute von Fahrzeug

---
+++ Code/src/main/java/model/fahrzeug/Fahrzeug.java
@@ -0,0 +1,23 @@
+package model.fahrzeug;
+import model.standort.Standort;
+import model.utils.Bild;
+
+public class Fahrzeug {
+    String hersteller;
+    String modell;
+    int baujahr;
+    boolean status;
+    int kilometerstand;
+    boolean reserviert;
+    Kennzeichen kennzeichen;
+    Ausrüstung[] ausrüstung;
+    Reifensatz reifensatz;
+    Standort standort;
+    Bild[] bilder;
+
+    public void Fahrzeug(){
+        
+    }
+
+
+}
---
+++ Code/src/main/java/model/standort/Standort.java
@@ -0,0 +1,4 @@
+package model.standort;
+
+public class Standort {
+}
