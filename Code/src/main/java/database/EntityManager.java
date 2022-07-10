fc60930a8c46498ab6553e3d22e922fc99c743f4
clemens1403 <Clemens.SRichter@t-online.de>
2022-07-10 13:07:45 +0200

    PUSH

---
+++ Code/src/main/java/database/EntityManager.java
@@ -0,0 +1,30 @@
+package database;
+
+import de.dhbwka.swe.utils.model.IPersistable;
+import de.dhbwka.swe.utils.util.GenericEntityManager;
+import model.fahrzeug.Fahrzeug;
+
+import java.util.List;
+
+public class EntityManager extends GenericEntityManager {
+
+    public EntityManager(){
+
+    }
+
+    public void modify(IPersistable el){
+
+    }
+
+    public void persistEl(IPersistable el){
+
+    }
+
+    public void removeEl(IPersistable el){
+
+    }
+
+    public List<IPersistable> getAllEl(Class c){
+        return [new Fahrzeug()];
+    }
+}
