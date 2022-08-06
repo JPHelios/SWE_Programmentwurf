5f755012a2d39b9e449ad021a016755558a86317
JPHelios <43265991+JPHelios@users.noreply.github.com>
2022-08-06 13:32:21 +0200

    Created Color Enum

---
+++ Code/src/main/java/util/Colors.java
@@ -0,0 +1,27 @@
+package util;
+
+import java.awt.*;
+
+public enum Colors {
+
+    PALE_VIOLET_RED(222,99,154),
+    CADILLAC_PINK(227,136,177),
+    PINK_ROSE(215,166,179),
+    PINK_MERCURY(241, 226, 226),
+    SMOKEY_GREY(112,112,112);
+
+    private final int r;
+    private final int g;
+    private final int b;
+
+    private Colors(final int r, final int g, final int b){
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+
+    public Color getColor(){
+        return new Color(r,g,b);
+    }
+
+}
