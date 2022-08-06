1776122739602e76cf48f5da6d2f95fc0408eb90
JPHelios <43265991+JPHelios@users.noreply.github.com>
2022-08-06 15:54:58 +0200

    Spagetti-Code vom Feinsten

--- Code/src/main/java/view/gui/FahrzeugGUI.java
+++ Code/src/main/java/view/gui/FahrzeugGUI.java
@@ -90,7 +90,7 @@ public class FahrzeugGUI extends GUIWindowComponent {
         JLabel mapLabel = new JLabel();
 
         ImageIcon icon = new ImageIcon("src\\main\\resources\\map.PNG");
-        icon = new ImageIcon(icon.getImage().getScaledInstance(900, 300, BufferedImage.SCALE_SMOOTH));
+        icon = new ImageIcon(icon.getImage().getScaledInstance(900, 500, BufferedImage.SCALE_SMOOTH));
 
         mapLabel.setIcon(icon);
         mapLabel.setHorizontalAlignment(JLabel.CENTER);
@@ -100,13 +100,15 @@ public class FahrzeugGUI extends GUIWindowComponent {
 
 //      #######################
         JPanel panelPanel = new JPanel();
-        panelPanel.setLayout(new GridLayout(5,2, 18,18));
+        panelPanel.setLayout(new GridLayout(5,2, 200,50));
+        panelPanel.setBackground(Colors.PINK_ROSE.getColor());
 
         JPanel statusPanel = new JPanel();
-        statusPanel.setBackground(Colors.CADILLAC_PINK.getColor());
+        statusPanel.setBackground(Colors.PINK_ROSE.getColor());
         statusPanel.setLayout(new BoxLayout(statusPanel, BoxLayout.Y_AXIS));
 
         JPanel buchungPanel = new JPanel();
+        buchungPanel.setBackground(Colors.PINK_ROSE.getColor());
         buchungPanel.setLayout(new BoxLayout(buchungPanel, BoxLayout.Y_AXIS));
 
         JPanel herstellerPanel = new JPanel();
@@ -114,13 +116,15 @@ public class FahrzeugGUI extends GUIWindowComponent {
         herstellerPanel.setLayout(new BoxLayout(herstellerPanel, BoxLayout.Y_AXIS));
 
         JPanel modellPanel = new JPanel();
+        modellPanel.setBackground(Colors.PINK_ROSE.getColor());
         modellPanel.setLayout(new BoxLayout(modellPanel, BoxLayout.Y_AXIS));
 
         JPanel klassenPanel = new JPanel();
-        klassenPanel.setBackground(Colors.CADILLAC_PINK.getColor());
+        klassenPanel.setBackground(Colors.PINK_ROSE.getColor());
         klassenPanel.setLayout(new BoxLayout(klassenPanel, BoxLayout.Y_AXIS));
 
         JPanel preisPanel = new JPanel();
+        preisPanel.setBackground(Colors.PINK_ROSE.getColor());
         preisPanel.setLayout(new BoxLayout(preisPanel, BoxLayout.Y_AXIS));
 
         JPanel baujahrPanel = new JPanel();
@@ -128,13 +132,15 @@ public class FahrzeugGUI extends GUIWindowComponent {
         baujahrPanel.setLayout(new BoxLayout(baujahrPanel, BoxLayout.Y_AXIS));
 
         JPanel kilometerPanel = new JPanel();
+        kilometerPanel.setBackground(Colors.PINK_ROSE.getColor());
         kilometerPanel.setLayout(new BoxLayout(kilometerPanel, BoxLayout.Y_AXIS));
 
         JPanel reifenPanel = new JPanel();
-        reifenPanel.setBackground(Colors.CADILLAC_PINK.getColor());
+        reifenPanel.setBackground(Colors.PINK_ROSE.getColor());
         reifenPanel.setLayout(new BoxLayout(reifenPanel, BoxLayout.Y_AXIS));
 
         JPanel ausruestungPanel = new JPanel();
+        ausruestungPanel.setBackground(Colors.PINK_ROSE.getColor());
         ausruestungPanel.setLayout(new BoxLayout(ausruestungPanel, BoxLayout.Y_AXIS));
 
         //----------------
@@ -228,18 +234,101 @@ public class FahrzeugGUI extends GUIWindowComponent {
 
         //----------------
 
-        panelPanel.add(statusPanel);
-        panelPanel.add(buchungPanel);
-        panelPanel.add(herstellerPanel);
-        panelPanel.add(modellPanel);
-        panelPanel.add(klassenPanel);
-        panelPanel.add(preisPanel);
-        panelPanel.add(baujahrPanel);
-        panelPanel.add(kilometerPanel);
-        panelPanel.add(reifenPanel);
-        panelPanel.add(ausruestungPanel);
+        JPanel layoutPanel1 = new JPanel();
+        layoutPanel1.setLayout(new BorderLayout());
+        layoutPanel1.add(statusPanel, BorderLayout.EAST);
+        layoutPanel1.setBackground(Colors.PINK_ROSE.getColor());
+
+        JPanel layoutPanel2 = new JPanel();
+        layoutPanel2.setLayout(new BorderLayout());
+        layoutPanel2.add(buchungPanel, BorderLayout.WEST);
+        layoutPanel2.setBackground(Colors.PINK_ROSE.getColor());
+
+        JPanel layoutPanel3 = new JPanel();
+        layoutPanel3.setLayout(new BorderLayout());
+        layoutPanel3.add(herstellerPanel, BorderLayout.EAST);
+        layoutPanel3.setBackground(Colors.PINK_ROSE.getColor());
+
+        JPanel layoutPanel4 = new JPanel();
+        layoutPanel4.setLayout(new BorderLayout());
+        layoutPanel4.add(modellPanel, BorderLayout.WEST);
+        layoutPanel4.setBackground(Colors.PINK_ROSE.getColor());
+
+        JPanel layoutPanel5 = new JPanel();
+        layoutPanel5.setLayout(new BorderLayout());
+        layoutPanel5.add(klassenPanel, BorderLayout.EAST);
+        layoutPanel5.setBackground(Colors.PINK_ROSE.getColor());
+
+        JPanel layoutPanel6 = new JPanel();
+        layoutPanel6.setLayout(new BorderLayout());
+        layoutPanel6.add(preisPanel, BorderLayout.WEST);
+        layoutPanel6.setBackground(Colors.PINK_ROSE.getColor());
+
+        JPanel layoutPanel7 = new JPanel();
+        layoutPanel7.setLayout(new BorderLayout());
+        layoutPanel7.add(baujahrPanel, BorderLayout.EAST);
+        layoutPanel7.setBackground(Colors.PINK_ROSE.getColor());
+
+        JPanel layoutPanel8 = new JPanel();
+        layoutPanel8.setLayout(new BorderLayout());
+        layoutPanel8.add(kilometerPanel, BorderLayout.WEST);
+        layoutPanel8.setBackground(Colors.PINK_ROSE.getColor());
+
+        JPanel layoutPanel9 = new JPanel();
+        layoutPanel9.setLayout(new BorderLayout());
+        layoutPanel9.add(reifenPanel, BorderLayout.EAST);
+        layoutPanel9.setBackground(Colors.PINK_ROSE.getColor());
+
+        JPanel layoutPanel10 = new JPanel();
+        layoutPanel10.setLayout(new BorderLayout());
+        layoutPanel10.add(ausruestungPanel, BorderLayout.WEST);
+        layoutPanel10.setBackground(Colors.PINK_ROSE.getColor());
+
+        panelPanel.add(layoutPanel1);
+        panelPanel.add(layoutPanel2);
+        panelPanel.add(layoutPanel3);
+        panelPanel.add(layoutPanel4);
+        panelPanel.add(layoutPanel5);
+        panelPanel.add(layoutPanel6);
+        panelPanel.add(layoutPanel7);
+        panelPanel.add(layoutPanel8);
+        panelPanel.add(layoutPanel9);
+        panelPanel.add(layoutPanel10);
 
         rightComponentPanel.add(panelPanel, BorderLayout.CENTER);
+
+        //########################
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setBackground(Colors.PINK_ROSE.getColor());
+        buttonPanel.setLayout(new GridLayout(1,7));
+
+        JLabel fillLabel1 = new JLabel(" ");
+        JLabel fillLabel2 = new JLabel(" ");
+        JLabel fillLabel3 = new JLabel(" ");
+        JLabel fillLabel4 = new JLabel(" ");
+        JLabel fillLabel5 = new JLabel(" ");
+
+        ButtonElement bearbeitenButton = ButtonElement.builder("BTN-BAR")
+                .buttonText("Bearbeiten")
+                .type(ButtonElement.Type.BUTTON)
+                .build();
+
+        ButtonElement loeschenButton = ButtonElement.builder("BTN-DEL")
+                .buttonText("Löschen")
+                .type(ButtonElement.Type.BUTTON)
+                .build();
+
+        buttonPanel.add(fillLabel1);
+        buttonPanel.add(bearbeitenButton);
+        buttonPanel.add(fillLabel2);
+        buttonPanel.add(fillLabel3);
+        buttonPanel.add(fillLabel4);
+        buttonPanel.add(loeschenButton);
+        buttonPanel.add(fillLabel5);
+
+
+        rightComponentPanel.add(buttonPanel, BorderLayout.SOUTH);
 
         gui.add(rightComponentPanel);
     }
