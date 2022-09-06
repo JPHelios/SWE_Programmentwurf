bd62e0fcdee538dd47acd3da1315324c2fdcd317
clemens1403 <Clemens.SRichter@t-online.de>
2022-09-06 22:48:02 +0200

    Update RabattaktionBuilder.java

--- Code/src/main/java/util/RabattaktionBuilder.java
+++ Code/src/main/java/util/RabattaktionBuilder.java
@@ -1,6 +1,9 @@
 package util;
 
+import app.Carsharing;
 import model.buchung.Rabattaktion;
+import model.fahrzeug.Ausruestung;
+import model.fahrzeug.Fahrzeugklasse;
 
 public class RabattaktionBuilder {
     private Rabattaktion r;
@@ -16,6 +19,16 @@ public class RabattaktionBuilder {
 
     public RabattaktionBuilder beschreibung(String b){
         r.setBeschreibung(b);
+        return this;
+    }
+
+    public RabattaktionBuilder aktionsklassen(String[] aIDs){
+        r.setAktionsKlassenIDs(aIDs);
+        Fahrzeugklasse[] a = new Fahrzeugklasse[aIDs.length];
+        for (int i = 0; i<aIDs.length; i++){
+            a[i] = Fahrzeugklasse.valueOf(aIDs[i]);
+        }
+        r.setAktionsKlassen(a);
         return this;
     }
 
